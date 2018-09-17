d5d5bfcb38d9e22ac023f307921526728877bb76
fahadhowlader <fahadhowlader@gmail.com>
2018-09-17 12:12:00 +0600

    animal_voices

--- proverbs.php
+++ proverbs.php
@@ -1,16 +1,16 @@
 <?php
 
-$newURL = "http://www.english-bangla.com/browse/proverbs";
-
-for ($i = 0; $i < 65; $i++) {
-    LoopTest($newURL, $i);
-}
+$newURL = "http://www.english-bangla.com/posts/animal_voices";
+ LoopTest($newURL, 1);
+//for ($i = 0; $i < 2; $i++) {
+//    LoopTest($newURL, $i);
+//}
 
 function LoopTest($main_url, $page_no) {
-    $previous_page_no = $page_no;
-    $url = $main_url . "/" . ($page_no + 1);
+//    $previous_page_no = $page_no;
+//    $url = $main_url . "/" . ($page_no + 1);
 
-    $html = file_get_contents($url); //get the html returned from the following url
+    $html = file_get_contents($main_url); //get the html returned from the following url
 //echo $html;
     $pokemon_doc = new DOMDocument();
 
@@ -24,17 +24,19 @@ function LoopTest($main_url, $page_no) {
 
         //get all the h2's with an id
 //	$pokemon_row = $pokemon_xpath->query('//h2[@id]');
-        $pokemon_row = $pokemon_xpath->query('//div[@class="proverbs"]/ul');
-
+        $pokemon_row = $pokemon_xpath->query('//div[@class="posts"]');
+   
         if ($pokemon_row->length > 0) {
             foreach ($pokemon_row as $row) {
-                $data_infos = $pokemon_xpath->query('//div[@class="proverbs"]/ul/li', $row);
-//                $data_span = $pokemon_xpath->query('//div[@class="proverbs"]/ul/li//span', $row);
+                $data_infos = $pokemon_xpath->query('//div[@class="posts"]//p', $row);
+//                $data_infos = $pokemon_xpath->query('//p', $row);
+               
                 foreach ($data_infos as $data_info) {
                     $data[] = $data_info->nodeValue;
                 }
             }
         }
+    
         if (!empty($data)) {
             dbInsert($data);
         }
@@ -64,7 +66,7 @@ function dbInsert($data) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
 
-    $sql = "INSERT INTO tbl_proverbs2 (full_proverbs)
+    $sql = "INSERT INTO tbl_animal_voices (animal_voices)
 VALUES $b;";
 
     echo $sql;
